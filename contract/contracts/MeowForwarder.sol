587a67bc6171128afed62ebda6fd71606d63d5f6
minniejung <94359917+minniejung@users.noreply.github.com>
2025-05-07 20:06:11 +0900

    Delete contract/contracts/MeowForwarder.sol

--- contract/contracts/MeowForwarder.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
-
-import "@openzeppelin/contracts/metatx/ERC2771Forwarder.sol";
-
-contract MeowForwarder is ERC2771Forwarder {
-    constructor(string memory _name) ERC2771Forwarder(_name) {}
-}
